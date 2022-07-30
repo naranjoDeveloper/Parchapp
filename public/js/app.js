298c8df21fbf2a6d5bbdaaec0d952e0860e9210e
naranjoEscritorio <naranjo.developer@gmail.com>
2022-07-30 12:42:43 -0500

    IMAGEN AÑADIDA

--- public/js/app.js
+++ public/js/app.js
@@ -127,7 +127,6 @@ document.addEventListener("DOMContentLoaded", function () {
     .collection("posts")
     .get()
     .then((snapshot) => {
-      console.log('data');
       setupPosts(snapshot.docs);
     });
 
